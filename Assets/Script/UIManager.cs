62b7ae2c7a7d1609cc66f8fd9de0af06e86a388e
glorykim <glorykim1216@gmail.com>
2019-07-12 23:00:57 +0900

    로비 UI

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject OptionPopup;
+    public GameObject LobbyUI;
+    public GameObject InGameUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OpenOption()
+    {
+        OptionPopup.SetActive(true);
+    }
+
+    public void CloseOption()
+    {
+        OptionPopup.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        LobbyUI.SetActive(false);
+        InGameUI.SetActive(true);
+    }
+}
