6b76fdde2cc56058a48fe153fa2e0c1706b44b40
BCLEE <rircon@gmail.com>
2019-07-16 18:30:25 +0900

    트윈 추가 스킨 ui 수정 폰트 추가

--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -2,37 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Lofle.Tween;
 public class UIManager : MonoBehaviour
 {
     public GameObject OptionPopup;
     public GameObject LobbyUI;
     public GameObject InGameUI;
     public GameObject ScrollView;
-    Dropdown m_Dropdown;
-    List<string> m_DropOptions;
-    int _iSkinNum=1;
+    public TweenPosition tween;
     // Start is called before the first frame update
     void Start()
     {
-        m_Dropdown = ScrollView.GetComponent<Dropdown>();
-        m_DropOptions = new List<string> { "Skin 1", "Skin 2", "Skin 3", "Skin 4", "Skin 5" };
-        m_Dropdown.ClearOptions();
+       
       
-        m_Dropdown.AddOptions(m_DropOptions);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
-            //스킨획득시 이코드 실행(해당 리스트는 DB로 관리되어야됨)
-            //_iSkinNum++;
-            //m_DropOptions.Add("Skin " + _iSkinNum);
-            
-            //로비화면 보일때
-
+            if(tween.isForward)
+                tween.PlayReverse();
+            else
+                tween.PlayForward();
         }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+           
+        }
+    }
+
+    public void SkinButton()
+    {
+        if (tween.isForward)
+        {
+            tween.PlayReverse();
+        }
+          
+        else
+        {
+            tween.PlayForward();
+        }
+           
     }
 
     public void SkinSelectedValue()
