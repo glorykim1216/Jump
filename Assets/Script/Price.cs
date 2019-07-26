54bda479c7c4f45a161e42946f493c5de757a968
BCLEE <rircon@gmail.com>
2019-07-26 18:02:21 +0900

    스킨완료 요청사항 - 골드 미터 가끔 100000000 찍힘 이전 기록 장벽 위치가 이상함 확인 요망

---
+++ Assets/Script/Price.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Price : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        this.GetComponentInChildren<Text>().text = UIManager.Instance.priceTemp.ToString();
+        this.GetComponentInChildren<RawImage>().texture = UIManager.Instance.renderTemp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -35,17 +35,24 @@ public class UIManager : MonoSingleton<UIManager>
     public List<int> skinBuyList;
     CheckSkin[] CheckSkinData;
 
+    public GameObject BuyIMG;
+    public int priceTemp;
+    public Texture renderTemp;
+    public GameObject ObjTemp;
+    public int SkinNumTemp;
+   
     public override void Init(){}
 
     private void Awake()
     {
-        
+     
     }
 
     void Start()
     {
         //tween 시작시 2번클릭 방지
         //tween.PlayReverse();
+        
         SkinScrollViewObj.SetActive(true);
         vibrationBtn.onClick.AddListener(VibrationOption);
         gameOverGoldBtn.onClick.AddListener(ViewAD);
@@ -53,11 +60,13 @@ public class UIManager : MonoSingleton<UIManager>
         VibrationOnOffCheck();
 
 
-        
-        GameManager.Instance.OpenSkinList = 231;
+
+        //GameManager.Instance.OpenSkinList = 231;
+        Debug.Log("GameManager.Instance.OpenSkinList" + GameManager.Instance.OpenSkinList);
         string test;
-        test = string.Format("{0:d20}", int.Parse(System.Convert.ToString(GameManager.Instance.OpenSkinList, 2)));
+        test = System.Convert.ToString(GameManager.Instance.OpenSkinList, 2).PadLeft(20, '0');
         Debug.Log(test);
+      
 
         CheckSkinData = this.gameObject.GetComponentsInChildren<CheckSkin>();
 
@@ -88,9 +97,9 @@ public class UIManager : MonoSingleton<UIManager>
                 CheckSkinData[i].CheckImg.SetActive(true);
         }
 
-
+        BuyIMG.SetActive(false);
         SkinScrollViewObj.SetActive(false);
-
+        GameObject.Find("Player").GetComponentInChildren<SkinnedMeshRenderer>().material = Resources.Load("Material/Icem" + GameManager.Instance.CurrSkin.ToString()) as Material;
     }
     string Num;
     // Update is called once per frame
@@ -98,45 +107,11 @@ public class UIManager : MonoSingleton<UIManager>
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            CheckSkinData = this.gameObject.GetComponentsInChildren<CheckSkin>();
-            Num = null;
-            foreach (CheckSkin k in CheckSkinData)
-            {
-                if (k.BuyCheck)
-                    Num += "1";
-                else
-                    Num += "0";
-            }
-
-            //Debug.Log(System.Convert.ToInt32(Num,2));
-
-            Debug.Log(Num);
+            Debug.Log(GameManager.Instance.CurrSkin);
         }
-        //if (Input.GetKeyDown(KeyCode.B))
-        //{
-        //    int testNum = 2;
-
-        //    string test;
-        //    test = string.Format("{0:d20}", int.Parse(System.Convert.ToString(testNum, 2)));
-        //    Debug.Log(test);
-
-        //    CheckSkinData = this.gameObject.GetComponentsInChildren<CheckSkin>();
-
-
-        //    for(int i = 0; i< CheckSkinData.Length; i++)
-        //    {
-        //        if(test[i].ToString() == "1")
-        //        {
-        //            Debug.Log("!!!!");
-        //            CheckSkinData[i].BuyCheck = true;
-        //        }
-        //        else
-        //            CheckSkinData[i].BuyCheck = false;
-        //    }
-          
-        //}
+        
     }
-
+   
     public void CheckSkin(int CurrentSkin)
     {
         if(EventSystem.current.currentSelectedGameObject.GetComponent<CheckSkin>().BuyCheck)
@@ -146,52 +121,116 @@ public class UIManager : MonoSingleton<UIManager>
                 k.CheckImg.SetActive(false);
             }
             GameManager.Instance.CurrSkin = CurrentSkin;
-
+           
             EventSystem.current.currentSelectedGameObject.GetComponent<CheckSkin>().CheckImg.SetActive(true);
+
+            //구입후 이미지 변환 작성해야됨;
+            
+            GameObject.Find("Player").GetComponentInChildren<SkinnedMeshRenderer>().material = Resources.Load("Material/Icem" + GameManager.Instance.CurrSkin.ToString()) as Material;
+            //            GameObject.Find("Player").GetComponentInChildren<SkinnedMeshRenderer>().material = 
+
+        }
+        else
+        {
+            priceTemp = EventSystem.current.currentSelectedGameObject.GetComponent<CheckSkin>().needMoney;
+            renderTemp = EventSystem.current.currentSelectedGameObject.transform.parent.GetComponentInChildren<RawImage>().texture;
+            ObjTemp = EventSystem.current.currentSelectedGameObject;
+            SkinNumTemp = CurrentSkin;
+            BuyIMG.SetActive(true);
+            //BuyIMG.transform.Find("price").GetComponent<Text>().text = EventSystem.current.currentSelectedGameObject.GetComponent<CheckSkin>().needMoney.ToString();
+
+         
         }
         Debug.Log("CurrentSkin : " + GameManager.Instance.CurrSkin);
-
+        Debug.Log("priceTemp : " + priceTemp);
         //Debug.Log(EventSystem.current.currentSelectedGameObject.name);
+    }
+
+    public void Purchase()
+    {
+        if( GameManager.Instance.Gold > ObjTemp.GetComponent<CheckSkin>().needMoney)
+        {
+            GameManager.Instance.Gold -= ObjTemp.GetComponent<CheckSkin>().needMoney;
+
+            ObjTemp.GetComponent<CheckSkin>().BuyCheck = true;
+
+            Num = null;
+            foreach (CheckSkin k in CheckSkinData)
+            {
+                if (k.BuyCheck)
+                    Num += "1";
+                else
+                    Num += "0";
+            }
+            /////
+            //저장
+            GameManager.Instance.OpenSkinList = System.Convert.ToInt32(Num, 2);
+            string test;
+            test = System.Convert.ToString(GameManager.Instance.OpenSkinList, 2).PadLeft(20, '0');
+            Debug.Log(test);
+
+            foreach (CheckSkin k in CheckSkinData)
+            {
+
+                k.CheckImg.SetActive(false);
+
+            }
+
+            for (int i = 0; i < CheckSkinData.Length; i++)
+            {
+                CheckSkinData[i].SkinNumber = i + 1;
+                if (test[i].ToString() == "1")
+                {
+                    Debug.Log("!!!!");
+                    CheckSkinData[i].moneyText.text = "OK        ";
+                    CheckSkinData[i].moneyImg.enabled = false;
+                    CheckSkinData[i].BuyCheck = true;
+
+                }
+                else
+                {
+                    CheckSkinData[i].BuyCheck = false;
+                }
+
+                if (CheckSkinData[i].SkinNumber == GameManager.Instance.CurrSkin)
+                    CheckSkinData[i].CheckImg.SetActive(true);
+            }
+
+            BuyIMG.SetActive(false);
+            ////
+
+
+            foreach (CheckSkin k in CheckSkinData)
+            {
+                k.CheckImg.SetActive(false);
+            }
+            GameManager.Instance.CurrSkin = SkinNumTemp;
+
+            ObjTemp.GetComponent<CheckSkin>().CheckImg.SetActive(true);
+
+
+            //구입후 이미지 변환 작성해야됨;
+            GameObject.Find("Player").GetComponentInChildren<SkinnedMeshRenderer>().material = Resources.Load("Material/Icem" + GameManager.Instance.CurrSkin.ToString()) as Material;
+        }
     }
 
     public void SkinButton()
     {
-        //트윈 위치 설정 (0,0,0,)으로 위치 이동하는 현상 제거
-        //tween.From = new Vector3(tween.transform.localPosition.x, 300, 0);
-        //tween.To = new Vector3(tween.transform.localPosition.x, 0, 0);
-        //if (tween.isForward)
-        //{
-        //    ArrowRot.localRotation = Quaternion.Euler(new Vector3(0, 0, -50));
-        //    tween.PlayReverse();
-        //}
-
-        //else
-        //{
-        //    ArrowRot.localRotation = Quaternion.Euler(new Vector3(0, 0, 130));
-        //    tween.PlayForward();
-        //}
         if(!SkinScrollViewObj.activeSelf)
             SkinScrollViewObj.SetActive(true);
         else
+        {
+            BuyIMG.SetActive(false);
             SkinScrollViewObj.SetActive(false);
+        }
+            
     }
-
+    public void BackButton()
+    {
+        BuyIMG.SetActive(false);
+    }
     public void EffectButton()
     {
-        //트윈 위치 설정 (0,0,0,)으로 위치 이동하는 현상 제거
-        //tween.From = new Vector3(tween.transform.localPosition.x, 300, 0);
-        //tween.To = new Vector3(tween.transform.localPosition.x, 0, 0);
-        //if (tween.isForward)
-        //{
-        //    ArrowRot.localRotation = Quaternion.Euler(new Vector3(0, 0, -50));
-        //    tween.PlayReverse();
-        //}
-
-        //else
-        //{
-        //    ArrowRot.localRotation = Quaternion.Euler(new Vector3(0, 0, 130));
-        //    tween.PlayForward();
-        //}
         if (!EffectScrollViewObj.activeSelf)
             EffectScrollViewObj.SetActive(true);
         else
